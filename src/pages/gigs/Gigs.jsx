87f97fa229679e575b17bafb2b2ce2d337cf615b
gradan-hash <mutukunyaa@gmail.com>
2023-09-18 18:38:05 +0300

    scss

--- src/pages/gigs/Gigs.jsx
+++ src/pages/gigs/Gigs.jsx
@@ -14,15 +14,15 @@ function Gigs() {
     setOpen(false);
   };
 
-  const apply = ()=>{
-    console.log(minRef.current.value)
-    console.log(maxRef.current.value)
-  }
+  const apply = () => {
+    console.log(minRef.current.value);
+    console.log(maxRef.current.value);
+  };
 
   return (
     <div className="gigs">
       <div className="container">
-        <span className="breadcrumbs">Liverr > Graphics & Design ></span>
+        <span className="breadcrumbs">Liverr Graphics & Design </span>
         <h1>AI Artists</h1>
         <p>
           Explore the boundaries of art and technology with Liverr's AI artists
@@ -46,13 +46,14 @@ function Gigs() {
                   <span onClick={() => reSort("createdAt")}>Newest</span>
                 ) : (
                   <span onClick={() => reSort("sales")}>Best Selling</span>
-                  )}
-                  <span onClick={() => reSort("sales")}>Popular</span>
+                )}
+                <span onClick={() => reSort("sales")}>Popular</span>
               </div>
             )}
           </div>
         </div>
         <div className="cards">
+          <span>radaaaa</span>
           {gigs.map((gig) => (
             <GigCard key={gig.id} item={gig} />
           ))}
